2499c250f5b43f026d4504b85692d76054c90a62
Rafaela Sacconi <rafaelassacconi@gmail.com>
2024-10-24 19:03:37 -0300

    feat: add has_group

--- womakers/surveys/templatetags/user_permissions.py
+++ womakers/surveys/templatetags/user_permissions.py
@@ -2,13 +2,17 @@ from django import template
 
 register = template.Library()
 
+@register.filter
 def is_editor(user):
     """ Checks if user is an editor """
     return user.groups.filter(name='Editor').exists()
 
+@register.filter
 def is_author(user):
     """ Checks if user is an editor """
     return user.groups.filter(name='Autor').exists()
 
-register.filter("is_editor", is_editor)
-register.filter("is_author", is_author)
+@register.filter
+def has_group(user, group_name):
+    """ Checks if user is an editor """
+    return user.groups.filter(name=group_name).exists()
